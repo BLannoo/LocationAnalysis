1dcbd78ed9449e173bb2da7d2372b69cdcaec7ce
BLannoo <brunolannoo@gmail.com>
2019-02-08 08:16:04 +0100

    simplify the loading, enhancing and filtering of the gps data

--- src/utils.py
+++ src/utils.py
@@ -13,34 +13,45 @@ BELGIUM_ROADS = gpd.read_file("../data/raw/Belgium/belgium-roads-shape/roads.shp
 BELGIUM_ROADS.crs = {"init": "epsg:4326"}
 
 
+# deprecated: only enhance the data if you need it
 def load_data(data_file_name: str) -> pd.DataFrame:
+    df = load_primary_data(data_file_name)
+    df = enhance_with_derived_data(df)
+    return df
+
+
+def load_primary_data(data_file_name: str):
     with open(data_file_name) as data_file:
         data_json = json.load(data_file)
-
     df = pd.DataFrame({
-        "raw": data_json["locations"],
         "timestampMs": [int(p["timestampMs"]) for p in data_json["locations"]],
         "latitudeE7": [p["latitudeE7"] for p in data_json["locations"]],
         "longitudeE7": [p["longitudeE7"] for p in data_json["locations"]],
         "accuracy": [p["accuracy"] for p in data_json["locations"]]
     })
+    return df
 
-    # Derivatives
+
+def enhance_with_derived_data(df: pd.DataFrame) -> pd.DataFrame:
     df["date"] = pd.to_datetime(df.timestampMs, unit="ms")
     df["year"] = pd.DatetimeIndex(df.date).year
     df["month"] = pd.DatetimeIndex(df.date).month
     df["day"] = pd.DatetimeIndex(df.date).day
     df["hour"] = pd.DatetimeIndex(df.date).hour
 
-    df["time_of_day"] = (df.timestampMs % (1000*60*60*24)) / (1000*60*60)
-    df["date_diff"] = df.timestampMs.diff()*-1
+    df["time_of_day"] = (df.timestampMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     df["duration"] = (df.timestampMs.shift(1) - df.timestampMs.shift(-1)) / 2 / (1000 * 60 * 60)
 
     return df
 
 
+# deprecated: load the data yourself
 def load_geo_data(data_file_name: str) -> gpd.GeoDataFrame:
     df = load_data(data_file_name)
+    return transform_to_geo_data(df)
+
+
+def transform_to_geo_data(df):
     gdf = gpd.GeoDataFrame(
         df, geometry=[
             Point(xy)
@@ -50,10 +61,31 @@ def load_geo_data(data_file_name: str) -> gpd.GeoDataFrame:
             )
         ]
     )
-
     # Setting the geometry by hand: http://geopandas.org/projections.html
     gdf.crs = {"init": "epsg:4326"}
     return gdf
+
+
+def filter_points_in_belgium(gdf: pd.DataFrame) -> gpd.GeoDataFrame:
+    if type(gdf) != gpd.GeoDataFrame:
+        gdf = transform_to_geo_data(gdf)
+
+    world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+    world.rename(columns={"name": "country"}, inplace=True)
+    world.drop(
+        ['pop_est', 'gdp_md_est', 'iso_a3']
+        , axis=1, inplace=True
+    )
+
+    gdf_with_countries = gpd.sjoin(gdf, world, how="inner", op="intersects")
+
+    return gdf_with_countries[gdf_with_countries["country"] == "Belgium"]
+
+
+def enhance_with_countries(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+    world.crs = {"init": "epsg:4326"}
+    return gpd.sjoin(gdf, world, how="inner", op="intersects")
 
 
 def show_data_density(
