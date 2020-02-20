a71da4523a8faa52340a5dbc883f72717ed7fe91
BLannoo <brunolannoo@gmail.com>
2020-02-20 22:00:27 +0100

    extract transform_to_geo_data to python and reverse durations for them to be positive

--- src/tools/pre_processing.py
+++ src/tools/pre_processing.py
@@ -1,5 +1,7 @@
 import json
 import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
 
 def json_to_csv(input_file_name: str) -> pd.DataFrame:
     with open(input_file_name) as file:
@@ -42,7 +44,21 @@ def enrich_data(df: pd.DataFrame) -> pd.DataFrame:
     ) / (1000 * 60 * 60)
     
     df["duration"] = (
-        df.timestampMs.shift(1) - df.timestampMs.shift(-1)
+        df.timestampMs.shift(-1) - df.timestampMs.shift(1)
     ) / 2 / (1000 * 60 * 60)
 
     return df
+
+def transform_to_geo_data(df: pd.DataFrame) -> gpd.GeoDataFrame:
+    gdf = gpd.GeoDataFrame(
+        df, geometry=[
+            Point(xy)
+            for xy in zip(
+                df.longitudeE7 / 10_000_000,
+                df.latitudeE7 / 10_000_000
+            )
+        ]
+    )
+    # Setting the geometry by hand: http://geopandas.org/projections.html
+    gdf.crs = {"init": "epsg:4326"}
+    return gdf
